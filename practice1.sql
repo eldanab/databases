489d835b2c9dcc7cf3010f85bcc08c1b9d821236
eldanab <y_balseit@kbtu.kz>
2024-09-17 17:40:08 +0500

    lab1

--- practice1.sql
+++ practice1.sql
@@ -1,1 +1,27 @@
-create database lab1
+create database lab1;
+
+create table users (
+    id serial,
+    firstname varchar(50),
+    lastname varchar(50)
+);
+
+alter table users add column
+    isadmin integer;
+
+alter table users
+    alter column isadmin type boolean using (isadmin = 1);
+
+alter table users
+    alter column isadmin set default false;
+
+
+alter table users
+    add constraint primary_key PRIMARY KEY(id);
+
+create table tasks(
+    id serial,
+    name varchar(50),
+    user_id integer
+);
+
